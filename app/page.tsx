336c08c453cbec8a9831095cbc4e0cf50be3b4bf
Mitko <mitkoiliev505@gmail.comm>
2023-10-25 13:39:43 +0200

    /

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Generator from '@/components/generator-body';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center bg-slate-900 justify-center p-8'>
+    <main className='flex min-h-screen flex-col items-center bg-slate-900 justify-center py-16 px-6'>
       <Generator />
     </main>
   );
