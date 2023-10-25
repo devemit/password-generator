59ff60d0a6033f21e14cf496822a41d192ae12e2
Mitko <mitkoiliev505@gmail.comm>
2023-10-25 13:32:35 +0200

    relocated to center

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Generator from '@/components/generator-body';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center bg-slate-900  p-12'>
+    <main className='flex min-h-screen flex-col items-center bg-slate-900 justify-center'>
       <Generator />
     </main>
   );
